7ef7e4033389e0bc15f619c8eb5d705a240f4e4b
habibaudu <auduhabib1990@gmail.com>
2019-03-09 20:43:30 +0100

    modified Mock files to pass test [#164495469]

--- src/Cart/types.js
+++ src/Cart/types.js
@@ -1,4 +1,4 @@
-const MAKE_SALES = 'MAKE_SALES'
-const MAKE_SALES_ERROR = 'MAKE_SALES'
+const MAKE_SALES = 'MAKE_SALES';
+const MAKE_SALES_ERROR = 'MAKE_SALES';
 
-export  default { MAKE_SALES, MAKE_SALES_ERROR }
+export default { MAKE_SALES, MAKE_SALES_ERROR };
