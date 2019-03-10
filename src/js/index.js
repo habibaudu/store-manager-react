ff9f454bcb7dfa0c3b85652524608b297e61d6bd
habibaudu <auduhabib1990@gmail.com>
2019-03-10 03:12:17 +0100

    remove invalid export [#164519988]

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-export function navSlide() {
+const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
   const navLinks = document.querySelectorAll('.nav-links li');
@@ -17,4 +17,4 @@ export function navSlide() {
 
     burger.classList.toggle('toggle');
   });
-}
+};
